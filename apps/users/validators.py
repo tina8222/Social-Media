222f47128165034134a8496051a8456a5c8cc39a
batman <naseri.mani2005@gmail.com>
2026-06-06 16:38:32 +0330

    feat(validators): update user validator

--- apps/users/validators.py
+++ apps/users/validators.py
@@ -1,9 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 User = get_user_model()
 
 def validator_target_user(username):
+    if not username:
+        error = {"error": "username is required"}
+        return Response(error, status=HTTP_400_BAD_REQUEST)
+
     try:
         return User.objects.get(username=username)
     except User.DoesNotExist:

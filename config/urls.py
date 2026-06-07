dda2c16087597b7fa3ff4efce3e0949b47db7bd8
batman <naseri.mani2005@gmail.com>
2026-06-07 12:23:26 +0330

    feat(urls): include like app's urls to the config dir

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
     path('posts/',include("apps.post.urls")),
+    path('like/', include("apps.like.urls"))
 ]
 
 if settings.DEBUG:

0a117279d2cc46667ab36f667a58c44e01475ba8
Tina <tinamirdar82@gmail.com>
2026-06-09 21:37:22 +0330

    feat(comment):implement PostCommentView CommentRepliesView

--- apps/comment/selectors.py
+++ apps/comment/selectors.py
@@ -2,6 +2,11 @@ from django.shortcuts import get_object_or_404
 from apps.post.models import Post
 from .models import Comment
 
+ALLOWED_ORDERING = {
+    "newest": "-created_at",
+    "oldest": "created_at",
+}
+
 
 def get_post_by_id(*, post_id):
 
@@ -20,6 +25,7 @@ def get_comment_by_id(*, comment_id):
     return get_object_or_404(
         Comment.objects.select_related(
             "post",
+            "post__owner",
         ),
         id=comment_id,
     )
@@ -29,3 +35,16 @@ def get_comment_by_id(*, comment_id):
 def get_comment_by_id_for_owner(*,comment_id,owner):
 
     return get_object_or_404(Comment,id=comment_id,user=owner)
+
+
+
+
+
+def get_post_comments(*,post_id,ordering="oldest"):
+
+    return (Comment.objects.select_related("user").filter(post_id=post_id,parent=None).order_by(ALLOWED_ORDERING.get(ordering,"created_at")))
+
+
+def get_comment_replies(*,comment_id):
+
+    return (Comment.objects.select_related("user").filter(parent_id=comment_id).order_by("created_at"))

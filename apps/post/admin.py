cd1b3a50d4b0f4528992f01728cf89a45d9625ff
batman <naseri.mani2005@gmail.com>
2026-06-15 12:19:00 +0330

    feat: saved post admin panel

--- apps/post/admin.py
+++ apps/post/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from apps.post.models import Post, PostMedia
+from apps.post.models import Post, PostMedia, SavedPost
 
 admin.site.register(Post)
 admin.site.register(PostMedia)
+
+
+@admin.register(SavedPost)
+class SavedPostAdmin(admin.ModelAdmin):
+    list_display = ["user", "post", "created_at"]
+    search_fields = ["user"]
+
